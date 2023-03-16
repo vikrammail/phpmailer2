b061665ac398b00b7bd4f4a476e0bc1261124de2
vikrammail <121216954+vikrammail@users.noreply.github.com>
2023-03-16 14:17:39 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,33 @@
+<?php
+include 'booststrap.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>links</title>
+  
+  <meta name="description" content="here are most usefull links,educational links,amazing links ,quiz and other many services provided">
+  
+  <meta name="keywords" content=" about , our website about,web-hub,web,usefull links,amazing links,educational links,quiz ,questionns">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1559084139599025"
+     crossorigin="anonymous"></script>
+</head>
+<body>
+
+<
+ 
+  
+    <div class="mb-3 sm-8 mt-3">
+<h4>about us</h4>
+
+<p>     Many services have been provided to you in this website, under which it has been provided usefull links, educational links, entertainment links.         </p>
+<p>     We are engaged in the work of making this webstite more better and convenient.         </p>
+<p>     If there is any other complaint from our website, you can <a href="contact" type="button" class="btn btn-outline-primary">contact</a> us, and we will try to fix it as soon as possible.
+
+Thanks.         </p>
+</div>   
+<?php   include 'example.php'; ?>
+</body>
+</html>
